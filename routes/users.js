6b96195c102fa44a6f4b5192786b087a64802c2d
Prakash Mishra <sirprakash1234@gmail.com>
2021-08-05 23:15:08 +0530

    passport jwt

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ var passport = require('passport');
 var router = express.Router();
 router.use(bodyParser.json());
 
+var authenticate = require('../authenticate');
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -30,9 +31,11 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({_id: req.user._id})
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res) => {
