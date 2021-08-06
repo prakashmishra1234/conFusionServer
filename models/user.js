0d0f1d719874cb84eb6f106c80159e7198a3365d
Prakash Mishra <sirprakash1234@gmail.com>
2021-08-06 18:38:13 +0530

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -3,6 +3,14 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lasttname: {
+        type: String,
+        default: ''
+    },
     admin:   {
         type: Boolean,
         default: false
