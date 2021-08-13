06d4f1213af9b978b9baa9439dc80ede312e66e4
Prakash Mishra <sirprakash1234@gmail.com>
2021-08-14 00:26:10 +0530

    passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '924936811395563',
+        clientSecret: '4ee47ec82982122c909cf105064044d0'
+    }
+
 }
