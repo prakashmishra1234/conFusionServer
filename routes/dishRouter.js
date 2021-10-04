138357364ca83f0c4ca0d0cf8661eaf507464682
Prakash Mishra <pm9507041@gmail.com>
2021-10-04 18:53:46 +0530

    Modified routers

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -22,6 +22,7 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
+
 .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.create(req.body)
     .then((dish) => {
